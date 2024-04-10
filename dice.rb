e20b9f682c6479e87b54bf3ad78efe28f4f26671
ecastroa9418 <165231064+ecastroa9418@users.noreply.github.com>
2024-04-10 23:17:15 +0000

    creating the views folder

--- dice.rb
+++ dice.rb
@@ -1,16 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+  
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
-end
-  
-get("/zebra") do
-  "We must add a route for each path we want to support"
-end
-  
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
+  erb(:elephant)
 end
 
 get("/dice/2/6") do
