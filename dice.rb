57a6339bbd5495e77b4b7a83e1db0bd2fb79fead
ecastroa9418 <165231064+ecastroa9418@users.noreply.github.com>
2024-04-11 00:06:04 +0000

    With Layout

--- dice.rb
+++ dice.rb
@@ -9,7 +9,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  erb(:elephant)
+  erb(:elephant, {:layout => :wrapper})
 end
 
 get("/dice/2/6") do
@@ -19,7 +19,7 @@ get("/dice/2/6") do
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  erb(:two_six)
+  erb(:two_six, {:layout => :wrapper})
 end
 
 
@@ -28,18 +28,16 @@ get("/dice/2/10") do
   second_simulation = rand(1..10)
   result = first_simulation +  second_simulation
 
-  output = "You rolled #{first_simulation} and then #{second_simulation}, adding to #{result}"
+  @output = "You rolled #{first_simulation} and then #{second_simulation}, adding to #{result}"
 
-  "<h1>2d10</h1> 
-  <p>#{output}</p>"
+  erb(:two_ten, {:layout => :wrapper})
 end
 
 get("/dice/1/20") do
   only_simulation = rand(1..20)
-  output_1d20 = "You rolled #{only_simulation}"
+  @output_1d20 = "You rolled #{only_simulation}"
 
-  "<h1>1d20</h1> 
-  <p>#{output_1d20}</p>"
+  erb(:one_twenty, {:layout => :wrapper})
 end
 
 get("/dice/5/4") do
@@ -50,8 +48,7 @@ get("/dice/5/4") do
   fith_simulation_dice5d4 = rand(1..4)
   result_dice5d4 = first_simulation_dice5d4 +  second_simulation_dice5d4 + third_simulation_dice5d4 + fourth_simulation_dice5d4 + fith_simulation_dice5d4
 
-  output_5d4 = "You first number is  #{first_simulation_dice5d4}, your second #{second_simulation_dice5d4}, your third #{third_simulation_dice5d4}, your fourth #{fourth_simulation_dice5d4}, and your fifth #{fith_simulation_dice5d4}, adding to #{result_dice5d4}"
+  @output_5d4 = "You first number is  #{first_simulation_dice5d4}, your second #{second_simulation_dice5d4}, your third #{third_simulation_dice5d4}, your fourth #{fourth_simulation_dice5d4}, and your fifth #{fith_simulation_dice5d4}, adding to #{result_dice5d4}"
 
-  "<h1>5d4</h1> 
-  <p>#{output_5d4}</p>"
+  erb(:five_four, {:layout => :wrapper})
 end
