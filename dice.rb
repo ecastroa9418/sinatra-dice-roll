f37a9ac1523e2363bae9ba8af075cd99e66097d9
ecastroa9418 <165231064+ecastroa9418@users.noreply.github.com>
2024-04-11 00:45:15 +0000

    Final Result

--- dice.rb
+++ dice.rb
@@ -9,46 +9,66 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  erb(:elephant, {:layout => :wrapper})
+  erb(:elephant)
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
 	
-  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  erb(:two_six, {:layout => :wrapper})
+  erb(:two_six)
 end
 
 
 get("/dice/2/10") do
-  first_simulation = rand(1..10)
-  second_simulation = rand(1..10)
-  result = first_simulation +  second_simulation
+  @rolls = []
 
-  @output = "You rolled #{first_simulation} and then #{second_simulation}, adding to #{result}"
+  2.times do
+    die = rand(1..10)
 
-  erb(:two_ten, {:layout => :wrapper})
+    @rolls.push(die)
+  end
+
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
-  only_simulation = rand(1..20)
-  @output_1d20 = "You rolled #{only_simulation}"
+  @rolls = []
 
-  erb(:one_twenty, {:layout => :wrapper})
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
-  first_simulation_dice5d4 = rand(1..4)
-  second_simulation_dice5d4 = rand(1..4)
-  third_simulation_dice5d4 = rand(1..4)
-  fourth_simulation_dice5d4 = rand(1..4)
-  fith_simulation_dice5d4 = rand(1..4)
-  result_dice5d4 = first_simulation_dice5d4 +  second_simulation_dice5d4 + third_simulation_dice5d4 + fourth_simulation_dice5d4 + fith_simulation_dice5d4
+  @rolls = []
 
-  @output_5d4 = "You first number is  #{first_simulation_dice5d4}, your second #{second_simulation_dice5d4}, your third #{third_simulation_dice5d4}, your fourth #{fourth_simulation_dice5d4}, and your fifth #{fith_simulation_dice5d4}, adding to #{result_dice5d4}"
+  5.times do
+    die = rand(1..4)
 
-  erb(:five_four, {:layout => :wrapper})
+    @rolls.push(die)
+  end
+
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
